9b11f0dc58f51ed72ae4c2b3f725bf91802c0e35
hernanex3 <hernan.digiorgi@gmail.com>
2018-01-24 11:09:57 -0300

    remote fun simplified and documented

--- remote_fun/Readme.md
+++ remote_fun/Readme.md
@@ -1,1 +1,19 @@
 # Remote function server
+
+## Problem
+Create an Erlang cluster with 2 nodes. One node is a server that just receives a function and become it.
+The other node sends a function the first and execute something remotely.
+
+## Solution
+The server (`remote_fun_server.erl`) receives a message as normal, and get the function from there. 
+The server also send a confirmation upon function reception to let the client know it was successful.
+
+The client (`remote_fun_client.erl`) creates a function that is able to receive and loop, and also
+to be killed, then it's send to the server.
+
+There is also a (`fun_common.hrl`), that holds common functions related to trying connect between nodes,
+this is for avoiding race conditions. Even if the use `.hrl` can be a bad practice, in this simple example
+without any project may be justifiable to show the `-include` and headers.
+
+Both, client and server are started with the `make` command, that spawns two named nodes using the same
+cookie. If the final output is `Ok!`, it works correctly. 
--- remote_fun/remote_fun_client.erl
+++ remote_fun/remote_fun_client.erl
@@ -1,12 +1,36 @@
 -module(remote_fun_client).
 
--export([start_function_client/0]).
+-export([function_client/0]).
 
-start_function_client() ->
-    
+-include("fun_common.hrl").
 
-%% internal functions
+function_client() ->
+    ?CONNECT_TO_SERVER,
+    send_function(fun multiply_loop/0),
+    ?SERVER_PID ! {self(), {3, 9}},
+    receive
+        {product_result, Result} ->
+            27 = Result
+    end,
+    ?SERVER_PID ! kill,
+    io:format("Ok!~n").
 
-add_function({A,B}) -> A + B.
-multiply_function({A,B}) -> A * B.
+%% because it can't be garanteed the order the nodes will be started,
+%% a confirmation is needed even for this simple exercise
+send_function(Function) ->
+    ?SERVER_PID ! {be, self(), Function},
+    receive
+        {function_server, ok} -> 
+            ok
+    after 1000 ->
+            send_function(Function)
+    end.
 
+multiply_loop() ->
+    receive
+        {From, {Factor1, Factor2}} ->
+            Result = Factor1 * Factor2,
+            From ! {product_result, Result},
+            multiply_loop();
+        kill -> kill
+    end.
