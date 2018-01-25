e54fbd1c8a0b4bbaf81de207d8a99ed1d58db56f
hernanex3 <hernan.digiorgi@gmail.com>
2018-01-25 10:35:44 -0300

    different handle method per clause

--- link_shorterner/src/shortener_link_handler.erl
+++ link_shorterner/src/shortener_link_handler.erl
@@ -4,27 +4,25 @@
 
 init(Req, State) ->
     Url = get_request_url(Req),
-    case cowboy_req:method(Req) of
-        <<"POST">> ->
-            {Status, ShortUrl} = handle_post(Url);
-        <<"GET">> ->
-            {Status, ShortUrl} = handle_get(Url)
-    end,
+    ReqMethod = cowboy_req:method(Req),
+    {Status, ShortUrl} = handle_request(ReqMethod, Url),
     Body = "{url: \"" ++ ShortUrl ++ "\"}",
     Header = #{<<"content-type">> => <<"application/json">>},
     Resp = cowboy_req:reply(Status, Header, Body, Req),
     {ok, Resp, State}.
 
-get_request_url(Req) ->
-    RequestPath = binary_to_list(cowboy_req:path(Req)),
-    string:substr(RequestPath, 2).
-
-handle_post(Url) ->
+handle_request(<<"POST">>, Url) -> 
     {_, ShortUrl} = shortener_shortener:short(Url),
-    {200, ShortUrl}.
-
-handle_get(Url) ->
+    {200, ShortUrl};
+    
+handle_request(<<"GET">>, Url) ->
     case shortener_shortener:get(Url) of
         not_found -> {204, ""};
         ShortUrl -> {200, ShortUrl}
     end.
+
+get_request_url(Req) ->
+    RequestPath = binary_to_list(cowboy_req:path(Req)),
+    string:substr(RequestPath, 2).
+
+    
