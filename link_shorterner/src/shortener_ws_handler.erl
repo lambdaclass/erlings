d9ad904a25fb3fe604151d08ce5e00531677f833
hernanex3 <hernan.digiorgi@gmail.com>
2018-01-25 13:44:11 -0300

    ws refactoring

--- link_shorterner/src/shortener_ws_handler.erl
+++ link_shorterner/src/shortener_ws_handler.erl
@@ -9,21 +9,15 @@ init(Req, Opts) ->
     {cowboy_websocket,Req,Opts}.
 
 websocket_init(State) ->
-    % inicializacion despues del upgrade, aqui estamos recien en websocket
-    io:format("inited~n"),
     shortener_shortener:subscribe(self()),
     {ok, State}.
 
-websocket_handle(Msg, State) ->
-    % handle received frames from the clients
-    io:format("ws handle: ~p~n", [Msg]),
+websocket_handle(_Msg, State) ->
     {ok, State}.
 
 websocket_info(#{long_url := LongUrl, short_url := ShortUrl}, State) ->
     Response = LongUrl++","++ShortUrl,
     {reply, {text, Response}, State};
 
-websocket_info(Info, State) ->
-    % info receives erlang msgs
-    io:format("ws info: ~p~n",[Info]),
+websocket_info(_Info, State) ->
     {ok, State}.
