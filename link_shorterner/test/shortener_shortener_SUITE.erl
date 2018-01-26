9dac1224775112540dde5f8dabad2adcdbb406e2
hernanex3 <hernan.digiorgi@gmail.com>
2018-01-26 14:08:24 -0300

    header test

--- link_shorterner/test/shortener_shortener_SUITE.erl
+++ link_shorterner/test/shortener_shortener_SUITE.erl
@@ -21,21 +21,23 @@ end_per_testcase(_, Config) ->
 
 test_notfound(_) ->
     NewUrl = gen_random_url(),
-    {404, #{}} = do_get_request(NewUrl).
+    {404, _,  #{}} = do_get_request(NewUrl).
 
 test_created(_) ->
     NewUrl = gen_random_url(),
-    {201, #{<<"url">> := _}} = do_post_request(NewUrl).
+    {201, _, #{<<"url">> := _}} = do_post_request(NewUrl).
 
 test_ok(_) ->
     LongUrl = gen_random_url(),
-    {201, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl),
-    {200, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl).
+    {201, _, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl),
+    {200, _, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl).
 
 test_redirect(_) ->
-    LongUrl = gen_random_url(),
-    {201, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl),
-    {302, #{<<"url">> := LongUrl}} = do_get_request(ShortUrl).
+    LongUrl = url("http://example.com/testing_redirect"),
+    {201, _, #{<<"url">> := ShortUrl}} = do_post_request(LongUrl),
+    {302, Headers, #{<<"url">> := LongUrl}} = do_get_request(ShortUrl),
+    RedirectionHeader = list_to_binary(proplists:get_value("location", Headers)),
+    LongUrl = RedirectionHeader.
 
 get_request_url(Url) ->
     BinaryReqUrl = iolist_to_binary([<<"http://localhost:8080/">>, Url]),
@@ -51,15 +53,15 @@ json_to_map(In) ->
 
 do_post_request(Url) ->
     ReqUrl = get_request_url(Url),
-    {ok, {{_, StatusCode, _}, _, Body}} =
+    {ok, {{_, StatusCode, _}, Headers, Body}} =
         httpc:request(post, {ReqUrl, [], [], []}, [], []),
-    {StatusCode, json_to_map(Body)}.
+    {StatusCode, Headers, json_to_map(Body)}.
 
 do_get_request(Url) ->
     ReqUrl = get_request_url(Url),
-    {ok, {{_, StatusCode, _}, _, Body}} =
+    {ok, {{_, StatusCode, _}, Headers, Body}} =
         httpc:request(get, {ReqUrl, []},[{autoredirect,false}],[]),
-    {StatusCode, json_to_map(Body)}.
+    {StatusCode, Headers, json_to_map(Body)}.
 
 start_web_server() ->
     application:ensure_all_started(shortener).
@@ -70,3 +72,7 @@ stop_web_server() ->
 gen_random_url() ->
     Random = base64:encode(crypto:strong_rand_bytes(10)),
     re:replace(Random, "/", "_", [global]).
+
+url(Url) ->
+    StringUrl = http_uri:encode(Url),
+    list_to_binary(StringUrl).
