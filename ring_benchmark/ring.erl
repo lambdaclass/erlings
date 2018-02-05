e6908e9dfe3286073c0d04fb5bc187dc8769503d
hernanex3 <hernan.digiorgi@gmail.com>
2018-02-05 10:43:04 -0300

    code simplified but slower

--- ring_benchmark/ring.erl
+++ ring_benchmark/ring.erl
@@ -1,54 +1,41 @@
 -module(ring).
 -export([main/1]).
 
-node_setup() -> node_setup(#{}).
-node_setup(State) ->
-    receive
-        {notify_time, Pid, MessageCount} ->
-            start_root_node(State#{pid => Pid, m => MessageCount});
-        {next_node, NextNode} ->
-            node_setup(State#{next_node => NextNode});
-        {send_message} ->
-            loop_normal_node(State)
-    end.
-
-start_root_node(State = #{next_node := NextNode}) ->
-    Now = erlang:timestamp(),
-    NextNode ! {send_message},
-    loop_root_node(State#{starting_time => Now}).
-
-loop_root_node(#{starting_time := StartingTime,
-                 pid := NotificationPid, m := 0}) ->
-    Now = erlang:timestamp(),
-    TimeDiffMs = timer:now_diff(Now, StartingTime),
-    NotificationPid ! {time, TimeDiffMs};
-
-loop_root_node(State = #{next_node := NextNode, m := MessageCount}) ->
-    NextNode ! {send_message},
-    receive
-        {send_message} -> loop_root_node(State#{m => MessageCount - 1})
-    end.
-
-loop_normal_node(State = #{next_node := NextNode}) ->
-    NextNode ! {send_message},
-    receive
-        {send_message} ->            
-            loop_normal_node(State)
-    end.
+node_loop() ->
+  receive
+    {msg, []} -> ok;
+    {msg, [FirstNode | OtherNodes]} ->
+      FirstNode ! {msg, OtherNodes},
+      node_loop()
+  end.
+    
 
 % N processes, M messages
 main([NArg,MArg]) ->
-    N = arg_to_number(NArg),
-    M = arg_to_number(MArg),
-    [Root | Rest] = [spawn_link(fun () -> node_setup() end)
-                     || _Time <- lists:seq(1, N)],
-    [Pid1 ! {next_node, Pid2}
-     || {Pid1, Pid2} <- lists:zip([Root | Rest], Rest ++ [Root])],
-    Root ! {notify_time, self(), M},
-    receive
-        {time, Time} -> Time
-    end,
-    io:format("Processes:~p, Messages:~p in ~pms~n", [N, M, Time]).
+  N = arg_to_number(NArg),
+  M = arg_to_number(MArg),
+  [FirstNode | Nodes] = create_processes(N, M),
+  BeforeFistMessage = os:timestamp(),
+  FirstNode ! {msg, Nodes},
+  receive
+    {msg, _} -> ok
+  end,
+  AfterLastMessage = os:timestamp(),
+  ElapsedTime = timer:now_diff(AfterLastMessage, BeforeFistMessage),
+  io:format("Processes: ~p, Messages ~p in ~pms~n", [N, M, ElapsedTime]).
+
+create_processes(N, M) ->
+  Processes = [spawn_link(fun () -> node_loop() end)
+               || _ <- lists:seq(1, N)],
+  ProcessList =
+    lists:foldl(fun(X,Acc) ->
+                    Acc ++ X
+                end,[],lists:duplicate(M, Processes)),
+  CompleteLoop = ProcessList ++ [self()],
+  lists:foldr(fun(Pid,_) ->
+                  Pid ! {process_list, CompleteLoop }
+              end,none,ProcessList),
+  CompleteLoop.
 
 arg_to_number(Arg) ->
     Str = atom_to_list(Arg),
