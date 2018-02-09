e95c04419a1f1f91f8811e2d1fa54b9e086dd28d
juan bono <juanbono94@gmail.com>
2018-02-09 19:07:23 -0300

    edit solutions

---
+++ sequential/lists/README.md
@@ -0,0 +1,17 @@
+# Lists
+
+## Exercises
+
+#### Recursion
+[Relevant chapter in Learn You Some Erlang](http://learnyousomeerlang.com/recursion)
+* [Reverse](reverse/)
+* [Remove consecutive](rmconsecutive/)
+* [Even Fibonacci Numbers](even_fib_numbers/)
+* [Reduce](reduce/)
+* [Rotate Lists](rotatelist/)
+* [Run-length Enconding](run_length_encoding/)
+
+#### Higher Order Functions
+[Relevant chapter in Learn You Some Erlang](http://learnyousomeerlang.com/higher-order-functions)
+* [Reduce](reduce/)
+* [any](any/)
---
+++ sequential/lists/solution/list.erl
@@ -0,0 +1,27 @@
+-module(list).
+
+-export([r/1,
+	my_foldl/3,
+	my_foldl/2],
+	even_fib_numbers/0,
+	list_any/1,
+	rm/1,
+	rotate/2,
+	run_length_encode/1).
+
+r(List) -> put_your_solution_here.
+
+my_foldl(Fun, Acc, List) -> put_your_solution_here.
+
+my_foldl(Fun, List) -> put_your_solution_here.
+
+even_fib_numbers() -> put_your_solution_here.
+
+list_any(N) ->
+    put_your_solution_here
+
+rm(List) -> put_your_solution_here.
+
+rotate(List, Tuple) -> put_your_solution_here.
+
+run_length_encode(List) -> put_your_solution_here.
