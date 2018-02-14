8d7fab784f4256171a1abc9bc976ef2834dd55d1
juan bono <juanbono94@gmail.com>
2018-02-14 16:51:27 -0300

    add shopping_cart exercise

---
+++ otp/shopping_cart/README.md
@@ -0,0 +1,26 @@
+# OTP
+
+## Shopping Cart
+[Relevant chapter in Learn You Some Erlang](http://learnyousomeerlang.com/clients-and-servers)
+
+In this exercise we are going to model a shopping cart. You can put items inside it (an item is just a tuple `{ItemName, Price}`) and ask him for the total cost of all the items.
+
+So, using a [gen_server](http://erlang.org/doc/man/gen_server.html), write the following functions:
+
+* `shopping_cart:start_link/0`: Start an empty shopping cart.
+* `shopping_cart:put_item/2`: Takes a PID and an item (a tuple) and saves it into the shopping cart.
+* `shopping_cart:cost_so_far/1`: Calculates the cost of all the items inside the shopping cart.
+* `finish/1`: Print the total price and finishes the shopping cart.
+
+* All the functions required by the behaviour.
+Example:
+``` erlang
+{ok, Pid} = shopping_cart:start_link().
+shopping_cart:put_item(Pid, {orange, 2}).
+%% [{orange, 2}]
+shopping_cart:put_item(Pid, {apple, 1}).
+%% [{apple, 1}, {orange, 2}]
+shopping_cart:cost_so_far(Pid).
+%% 3
+```
+[solution](src/solution/shopping_cart.erl)
---
+++ otp/shopping_cart/src/solution/shopping_cart.erl
@@ -0,0 +1,51 @@
+-module(shopping_cart).
+
+-behaviour(gen_server).
+
+-export([start_link/0,
+         put_item/2,
+         finish/1,
+         init/1,
+         handle_call/3,
+         terminate/2,
+         cost_so_far/1,
+	 handle_cast/2
+        ]).
+
+start_link() ->
+     gen_server:start_link(?MODULE, [], []).
+
+put_item(Pid, Item) ->
+    gen_server:call(Pid, {item, Item}).
+
+cost_so_far(Pid) ->
+    gen_server:call(Pid, cost).
+
+finish(Pid) ->
+    gen_server:call(Pid, terminate).
+
+%% Server functions
+init([]) ->
+    {ok, []}.
+
+handle_call({item, Item}, _From, Cart) ->
+    {reply, [Item|Cart], [Item|Cart]};
+
+handle_call(terminate, _From, Cart) ->
+    {stop, normal, ok, Cart};
+
+handle_call(cost, _From, Cart) ->
+    Total_Price = total_price(Cart),
+    {reply, Total_Price, Cart}.
+
+handle_cast(_, _) ->
+    {noreply, ok}.
+
+terminate(normal, Cart) ->
+    io:format("The total price was: ~p. ~n", [total_price(Cart)]),
+    ok.
+
+%% Private functions
+total_price(Cart) ->
+    Prices = lists:map(fun({_, Price}) -> Price end, Cart),
+    lists:sum(Prices).
