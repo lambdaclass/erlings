8b9693ed84737af477d78a362b8bc84a077eac49
juan bono <juanbono94@gmail.com>
2018-02-20 11:47:33 -0300

    add filter_fibonacci_numbers

--- sequential/filter_powers_of_2/src/filter_powers_of_2.erl
+++
@@ -1,6 +0,0 @@
--module(filter_powers_of_2).
-
--export([filter_powers/1]).
-
-filter_powers(List) -> 
-    put_your_solution_here.
--- sequential/filter_powers_of_2/test/filter_powers_of_2_test.erl
+++
@@ -1,9 +0,0 @@
--module(filter_powers_of_2_test).
-
-
--include_lib("eunit/include/eunit.hrl").
-
-filter_powers_of_2_test() ->
-    List = [1, 2, 3, 4, 5, 7, 8],
-    ?assertEqual([1, 2, 4, 8], filter_powers_of_2:filter_powers(List)).
-
