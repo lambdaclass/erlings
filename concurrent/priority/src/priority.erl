1adc3457f7b014e4062f83a5ba1c8bd5f138e702
Amin Arria <arria.amin@gmail.com>
2018-06-08 16:07:06 -0300

    Changed directory structure for Travis

---
+++ concurrent/priority/src/priority.erl
@@ -0,0 +1,14 @@
+-module(priority).
+
+-export([start/0,
+         get_messages/1,
+         priority_loop/1]).
+
+start() ->
+  your_solution_here.
+
+get_messages(Pid) ->
+  your_solution_here.
+
+priority_loop(State) ->
+  your_solution_here.
