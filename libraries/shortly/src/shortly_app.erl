a0053c9218f59089bb72743e170dd5d871e862e0
Amin Arria <arria.amin@gmail.com>
2018-06-11 16:03:51 -0300

    Dynamic ports and distributed nodes

--- libraries/shortly/src/shortly_app.erl
+++ libraries/shortly/src/shortly_app.erl
@@ -2,10 +2,11 @@
 
 -behaviour(application).
 
--export([start/2, 
+-export([start/2,
          stop/1]).
 
 start(_StartType, _StartArgs) ->
+    {ok, Port} = application:get_env(shortly, port),
     Dispatch = cowboy_router:compile(
                  [
                   {'_', [
@@ -13,11 +14,11 @@ start(_StartType, _StartArgs) ->
                          {"/:url", shortly_link_handler, []}
                         ]}
                  ]),
-    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+    {ok, _} = cowboy:start_clear(http, [{port, Port}], #{
 		env => #{dispatch => Dispatch}
 	}),
     shortly_sup:start_link().
 
-stop(_State) -> 
+stop(_State) ->
     cowboy:stop_listener(http),
     ok.
