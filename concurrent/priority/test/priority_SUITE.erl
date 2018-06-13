b2fd82bc135f379b18429fd5b6178f81f09c0233
Amin Arria <arria.amin@gmail.com>
2018-06-13 14:55:41 -0300

    Migrated to CT

---
+++ concurrent/priority/test/priority_SUITE.erl
@@ -0,0 +1,12 @@
+-module(priority_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0]).
+-export([run_eunit/1]).
+
+all() ->
+  [run_eunit].
+
+run_eunit(_Config) ->
+  ok = eunit:test(priority_test).
