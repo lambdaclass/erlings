d8686cc80fa9ea16be69846437d4636720e11dd5
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-10-18 18:02:58 -0300

    Maps done

--- sequential/maps/src/maps_exercises.erl
+++ sequential/maps/src/maps_exercises.erl
@@ -1,21 +1,32 @@
 -module(maps_exercises).
 
--export([list_to_map/1]).
+-record(person, {name, age}).
 
-merge(M1, M2) ->
-  put_your_solution_here.
+-export([map/2, maps_to_records/1, merge/2,
+	 proplist_to_map/1, records_to_maps/1, to_map/1]).
+
+merge(M1, M2) -> maps:fold(fun (K, V, Map) -> maps:put(K, V, Map) end, M1, M2).
 
 map(Function, Map) ->
-  put_your_solution_here.
+    maps:fold(fun (K, V, NewMap) -> maps:put(K, Function(V), NewMap) end, #{}, Map).
 
-to_map(List) ->
-  put_your_solution_here.
+to_map(List) -> to_map(List, 1, #{}).
+
+to_map([], _Index, Map) -> Map;
+to_map([Head | Tail], Index, Map) ->
+    to_map(Tail, Index + 1, maps:put(Index, Head, Map)).
 
 records_to_maps(Records) ->
-  put_your_solution_here.
+    lists:map(fun (#person{name = Name, age = Age}) -> #{name => Name, age => Age} end, Records).
 
-maps_to_records(Maps) ->
-  put_your_solution_here.
+maps_to_records(Maps) -> maps_to_records(Maps, []).
 
-proplist_to_map(Proplist) ->
-  put_your_solution_here.
+maps_to_records([], Persons) -> Persons;
+maps_to_records([#{age := Age, name := Name} | Tail], Persons) ->
+    maps_to_records(Tail, [#person{name = Name, age = Age} | Persons]).
+
+proplist_to_map(Proplist) -> proplist_to_map(Proplist, #{}).
+
+proplist_to_map([], Map) -> Map;
+proplist_to_map([{Key, Value} | Tail], Map) -> 
+    proplist_to_map(Tail, maps:put(Key, Value, Map)).
