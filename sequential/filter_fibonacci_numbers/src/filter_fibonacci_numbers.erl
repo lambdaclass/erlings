10e3bd22aa3f00065e136faabd67c5ec48c8e26c
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-10-18 18:30:59 -0300

    Refactore filter fib

--- sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
+++ sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
@@ -6,10 +6,9 @@ filter(List) ->
     FibList = fib_less_than(lists:max(List)),
     [X || X <- List, lists:member(X, FibList)].
 
-fib_less_than(N) ->
-    Fig_gen = fun Fib(I, J, Acc) ->
-		      if I =< N -> Fib(J, I + J, [I | Acc]);
-			 true -> Acc
-		      end
-	      end,
-    Fig_gen(1, 1, []).
+fib_less_than(N) -> fib_less_than(N, 1, 1, []).
+
+fib_less_than(N, Current, Next, Acc) when Current =< N -> 
+    fib_less_than(N, Next, Current + Next, [Current | Acc]);
+fib_less_than(_N, _Current, _Next, Acc) ->
+    Acc.
