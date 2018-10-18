303b8bcd78242a71c121fb6f5c1f5d438724e6e3
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-10-18 16:48:54 -0300

    Filter fibs done

--- sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
+++ sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
@@ -3,4 +3,13 @@
 -export([filter/1]).
 
 filter(List) ->
-  put_your_solution_here.
+    FibList = fib_less_than(lists:max(List)),
+    [X || X <- List, lists:member(X, FibList)].
+
+fib_less_than(N) ->
+    Fig_gen = fun Fib(I, J, Acc) ->
+		      if I =< N -> Fib(J, I + J, [I | Acc]);
+			 true -> Acc
+		      end
+	      end,
+    Fig_gen(1, 1, []).
