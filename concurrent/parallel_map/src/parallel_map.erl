9adf60ec42e3eea0596204d4fa1a37f1331198f7
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-10-19 11:08:45 -0300

    Pmap done

--- concurrent/parallel_map/src/parallel_map.erl
+++ concurrent/parallel_map/src/parallel_map.erl
@@ -1,5 +1,12 @@
 -module(parallel_map).
--export([pmap/2]).
+-export([pmap/2, mapper/1]).
+
+mapper(Value) -> 
+    receive
+        {From, {Fun}} -> From ! {self(), {Fun(Value)}}
+    end.
 
 pmap(Fun, List) ->
-  your_solution_here.
+    Workers = [spawn_link(?MODULE, mapper, [X]) || X <- List],
+    lists:foreach(fun (Worker) -> Worker ! {self(), {Fun}} end, Workers), 
+    lists:map(fun (Worker) -> receive {Worker, {Mapped}} -> Mapped end end, Workers).
