e7cdc67abf304e3f5cff63ee9cb615423ee960f0
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-10-22 12:20:58 -0300

    Ring benchmark done

--- concurrent/ring_benchmark/src/ring.erl
+++ concurrent/ring_benchmark/src/ring.erl
@@ -1,6 +1,29 @@
 -module(ring).
--export([ring/2]).
+-export([ring/2, worker/1]).
 
 %% N processes, M messages
-ring(_N, _M) ->
-  complete_here.
+ring(N, M) ->
+    io:format("Starting ~p processes~n", [N]),
+    Node = lists:foldl(fun create_process/2, self(), lists:seq(1, N)),
+    Msg = <<"hola">>,
+    do_rounds(M, Node, Msg).
+
+create_process(_X, Next) ->
+    spawn_link(?MODULE, worker, [Next]).
+
+worker(Next) ->
+    receive
+        Msg -> Next ! Msg,
+        worker(Next)
+    end.
+
+do_rounds(1, Node, Msg) ->
+    Node ! Msg,
+    receive
+        Msg -> io:format("Done!")
+    end;
+do_rounds(Rounds, Node, Msg) ->
+    Node ! Msg,
+    receive
+        Msg -> do_rounds(Rounds - 1, Node, Msg)
+    end.
