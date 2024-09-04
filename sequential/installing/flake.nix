98d005b06ee04416c79ea45c05bfd26bfd8d84f4
fkrause98 <fkrausear@gmail.com>
2024-09-04 12:45:08 -0300

    CI: try to use flake instead

--- sequential/installing/flake.nix
+++ sequential/installing/flake.nix
@@ -2,8 +2,8 @@
   description = "A flake to download Erlang 27 and Rebar3 from Nixpkgs 24.05";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.05"; # Pinning to Nixpkgs version 24.05
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    flake-utils.url = "github:numtide/flake-utils/main";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
@@ -13,8 +13,8 @@
       in {
         packages.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.erlang_27    # Erlang 27
-            pkgs.rebar3       # Rebar3
+            pkgs.erlang_27    
+            pkgs.rebar3       
           ];
         };
       });
